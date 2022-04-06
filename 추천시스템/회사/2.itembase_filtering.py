8e8d1bf5c8e37fb8af7a81006bf18136eb927bdf
kwonjh1412 <kwonkai383838@gmail.com>
2022-04-07 08:12:36 +0900

    추천시스템

--- 추천시스템/회사/2.itembase_filtering.py
+++ 추천시스템/회사/2.itembase_filtering.py
@@ -22,45 +22,54 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from tables import Column
  
 # user-item 테이블 필요
 # pivot table이용하여 만들기
-data = pd.read_csv('C:/kwon/juno1412/추천시스템/ratings.csv')
-data = data.pivot_table('rating', index='userId', columns='movieId')
-data
+data = pd.read_csv('추천시스템/score2.csv')
+data = data.pivot_table('score', index='userid', columns='movieCd')
+
 
 # 유저 정보, 영화정보 통합
-info = pd.read_csv('C:/kwon/juno1412/추천시스템/movies.csv')
-movie_data = pd.merge(data,info, on='movieId')
+info = pd.read_csv('추천시스템/movie_info_result.csv')
+info = info[['movieCd', 'movieNm', 'genreNm','directors_ko']]
+score = pd.read_csv('추천시스템/score2.csv')
+score = score[['score', 'userid']]
+movie_data = pd.concat([info, score], axis=1)
+movie_data
 
 # user-item 테이블 수정
-data2 = movie_data.pivot_table('rating', index='userId', columns='title').fillna(0)
-data2 = data2.transpose()
-data2
+data_t = movie_data.pivot_table('score', index='userid', columns='movieNm').fillna(0)
+data_t = data_t.transpose()
 
-sim = cosine_similarity(data2, data2)
-sim_df = pd.DataFrame(data = sim, index = data2.index, columns = data2.index)
+
+sim = cosine_similarity(data_t, data_t)
+sim_df = pd.DataFrame(data = sim, index = data_t.index, columns = data_t.index)
 sim_df
 
 # 영화의 유사도 50개
-sim_df["Nadja (1994)"].sort_values(ascending=False)[1:50] # 내림차순 정렬
+sim_df["스파이더맨: 노 웨이 홈"].sort_values(ascending=False)[1:50] # 내림차순 정렬
 
 # 영화 추천
 
 # 개인최적화 영화 추천
 # score_arr : user * item, sim_arr : item * item
 def predict_score(score_arr, sim_arr):
-    sum_sr = score_arr @ sim_arr # 행렬곱 2x2 = .dot # 유저평가, 아이템 유사도 행렬곱
-    sum_s_abs = np.array( [ np.abs(sim_arr).sum(axis=1)] ) # sim_arr 절댓값의 합 1차원 배열
+    ratings_pred = score_arr.dot(sim_arr)/ np.array([np.abs(sim_arr).sum(axis=1)]) 
+    return ratings_pred
+    # predict = score_arr.dot(sim_arr) / np.array([np.abs(sim_arr).sum(axis=1)])
+    # sum_sr = score_arr @ sim_arr # 행렬곱 2x2 = .dot # 유저평가, 아이템 유사도 행렬곱
+    # sum_s_abs = np.array( [ np.abs(sim_arr).sum(axis=1)] ) # sim_arr 절댓값의 합 1차원 배열
 
-    predict = sum_sr / sum_s_abs
-    return predict
+    # predict = sum_sr / sum_s_abs
+    # score_arr
+    # return predict
 
 #성능평가
+data_t.shape
+sim_df.shape
 
-predict = predict_score(data2.values, sim_df.values)
-predict_metrix = pd.DataFrame(data=predict_score, index = data2.columns, columns = data2.index)
+predict = predict_score(data_t.values, sim_df.values)
+predict_metrix = pd.DataFrame(data=predict_score, index = data_t.columns, columns = data_t.index)
 predict_metrix.head(3)
 
 from sklearn.metrics import mean_squared_error
@@ -71,5 +80,5 @@ def get_mse(pred, actual):
     
     return mean_squared_error(pred, actual)
 
-MSE1 = get_mse(predict_score, data2.values)
-print(f'아이템 기반 모든 인접 이웃 MSE: {MSE1:.4f}')
+MSE = get_mse(predict, predict_metrix.values)
+print(f'인접 이웃: {MSE:.4f}')
