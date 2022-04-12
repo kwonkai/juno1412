702d84fd797b202a93e1182fcd6b2707ad2d72f4
kwonjh1412 <kwonkai383838@gmail.com>
2022-04-13 00:44:14 +0900

    keras

---
+++ DL/모두의 딥러닝/4.DL4_keras1.py
@@ -0,0 +1,14 @@
+﻿# keras 함수 불러오기
+from keras.models import Sequential
+from keras.layers import Dense
+
+# 라이브러리 불러오기
+import numpy
+import tensorflow as tf
+tf.__version__
+
+# seed=0 설정 -> 실행할 때마다 같은 결과 출력
+seed=0
+numpy.random.seed(seed)
+tf.set_random_seed(seed)
+
