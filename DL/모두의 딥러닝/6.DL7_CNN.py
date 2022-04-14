bab28e3ade7ce22da051f983a6d206e5d60b7433
kwon <kwonkai383838@gmail.com>
2022-04-14 18:00:42 +0900

    CNN

--- DL/모두의 딥러닝/6.DL7_CNN.py
+++ DL/모두의 딥러닝/6.DL7_CNN.py
@@ -1,47 +1,68 @@
-﻿# CNN
+﻿from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.callbacks import ModelCheckpoint,EarlyStopping
 
-# 라이브러리 설정
-import pandas as pd
-import numpy as np
-import sys
-import tensorflow as tf
 import matplotlib.pyplot as plt
-
-from tensorflow.keras.datasets import mnist
-from tensorflow.python.keras.utils import np_utils
+import numpy
+import os
+import tensorflow as tf
 
 # seed 값 설정
 seed = 0
-np.random.seed(seed)
+numpy.random.seed(seed)
 tf.set_random_seed(seed)
 
-# 데이터 불러오기 & 데이터셋
-(x_train, y_class_train), (x_test, y_class_test) = mnist.load_data()
+# MNIST 데이터 불러오기
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
-print("학습셋 이미지 수 : %d 개" % (x_train.shape[0]))
-print("테스트세 이미지 수 : %d 개" % (x_test.shape[0]))
+X_train = X_train.reshape(X_train.shape[0], 784).astype('float32') / 255
+X_test = X_test.reshape(X_test.shape[0], 784).astype('float32') / 255
 
-plt.imshow(x_train[0], cmap='Greys')
+Y_train = np_utils.to_categorical(Y_train, 10)
+Y_test = np_utils.to_categorical(Y_test, 10)
+
+# 모델 프레임 설정
+model = Sequential()
+model.add(Dense(512, input_dim=784, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+# 모델 실행 환경 설정
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+# 모델 최적화 설정
+MODEL_DIR = './model/'
+if not os.path.exists(MODEL_DIR):
+    os.mkdir(MODEL_DIR)
+
+modelpath="./model/{epoch:02d}-{val_loss:.4f}.hdf5"
+checkpointer = ModelCheckpoint(filepath=modelpath, monitor='val_loss', verbose=1, save_best_only=True)
+early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10)
+
+# 모델의 실행
+history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=30, batch_size=200, verbose=0, callbacks=[early_stopping_callback,checkpointer])
+
+# 테스트 정확도 출력
+print("\n Test Accuracy: %.4f" % (model.evaluate(X_test, Y_test)[1]))
+
+# 테스트 셋의 오차
+y_vloss = history.history['val_loss']
+
+# 학습셋의 오차
+y_loss = history.history['loss']
+
+# 그래프로 표현
+x_len = numpy.arange(len(y_loss))
+plt.plot(x_len, y_vloss, marker='.', c="red", label='Testset_loss')
+plt.plot(x_len, y_loss, marker='.', c="blue", label='Trainset_loss')
+
+# 그래프에 그리드를 주고 레이블을 표시
+plt.legend(loc='upper right')
+# plt.axis([0, 20, 0, 0.35])
+plt.grid()
+plt.xlabel('epoch')
+plt.ylabel('loss')
 plt.show()
-
-# 코드로 확인
-for x in x_train[0]:
-    for i in x:
-        sys.stdout.write('%d\t' % i)
-    sys.stdout.write('\n')
-
-# 차원 변화 과정
-x_train = x_train.reshape(x_train.shape[0], 784)
-x_train = x_train.astype('float64')
-x_train = x_train/255
-
-x_test = x_test.reshape(x_test.shape[0], 784).astype('float64') / 255
-
-# 클래스 값 확인
-print("class : %d" % (y_class_train[0]))
-
-# 바이너리화 과정
-y_train = np_utils.to_categorical(y_class_train, 10)
-y_test = np_utils.to_categorical(y_class_test, 10)
-
-print(y_train[0])
