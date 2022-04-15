a81097ced08dd024b227b546be9795941f6e68be
kwon <kwonkai383838@gmail.com>
2022-04-15 17:36:15 +0900

    cnn_copy

--- DL/모두의 딥러닝/6.DL7_CNN_modeling copy.py
+++ DL/모두의 딥러닝/6.DL7_CNN_modeling copy.py
@@ -1,7 +1,7 @@
 ﻿from tensorflow.keras.datasets import mnist
-from tensorflow.keras.utils import np_utils
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.callbacks import ModelCheckpoint,EarlyStopping
 
 import matplotlib.pyplot as plt
@@ -17,26 +17,32 @@ tf.set_random_seed(seed)
 # MNIST 데이터 불러오기
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
+# 데이터 바꾸기
 X_train = X_train.reshape(X_train.shape[0], 784).astype('float64') / 255
 X_test = X_test.reshape(X_test.shape[0], 784).astype('float64') / 255
+Y_train = to_categorical(Y_train, 10)
+Y_test = to_categorical(Y_test, 10)
 
-Y_train = np_utils.to_categorical(Y_train, 10)
-Y_test = np_utils.to_categorical(Y_test, 10)
-
-# 모델 프레임 설정
+# 모델 프레임 설정/ / 컨볼루전 신경망(CNN)
 model = Sequential()
-model.add(Dense(512, input_dim=784, activation='relu'))
+model.add(Conv2D(32, kernel_size=(3,3), input_shape=(28,28,1), activation='relu'))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
 
 # 모델 컴파일
-model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # 모델 최적화 설정
-MODEL_DIR = 'C:/Users/kwonk/juno1412-1/juno1412/DL/모두의 딥러닝/model/'
+MODEL_DIR = 'C:/Users/kwonk/juno1412-1/juno1412/DL/모두의 딥러닝/model1/'
 if not os.path.exists(MODEL_DIR):
     os.mkdir(MODEL_DIR)
 
-modelpath='C:/Users/kwonk/juno1412-1/juno1412/DL/모두의 딥러닝/model/{epoch:02d}-{val_loss:.4f}.hdf5'
+modelpath='C:/Users/kwonk/juno1412-1/juno1412/DL/모두의 딥러닝/model1/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpointer = ModelCheckpoint(filepath=modelpath, monitor='val_loss', verbose=1, save_best_only=True)
 early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10)
 
