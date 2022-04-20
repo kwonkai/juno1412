afc4e10573dc0cbff2b51132dddfe5a1c24d8124
kwon <kwonkai383838@gmail.com>
2022-04-20 17:51:46 +0900

    web...

--- web.py
+++ web.py
@@ -1,20 +1,26 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer # python3
+import socketserver
+import json
+import cgi
+
 class HandleRequests(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
 
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+    def do_HEAD(self):
+        self._set_headers()
 
+    
+    # get으로 메세지 보내기
     def do_GET(self):
         self._set_headers()
         self.wfile.write("received get request")
-        
+    
+    # post로 json 폴더에 메세지 보내기
     def do_POST(self):
+        
         '''Reads post request body'''
         self._set_headers()
         content_len = int(self.headers.getheader('content-length', 0))
@@ -25,6 +31,6 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.do_POST()
 
 host = ''
-port = 8081
+port = 8082
 HTTPServer((host, port), HandleRequests).serve_forever()
 
