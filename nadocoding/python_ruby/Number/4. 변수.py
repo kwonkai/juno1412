db9bbd7e0de83051b79cdb5679f72f4ffa7360bd
kwon <kwonkai383838@gmail.com>
2022-04-26 15:03:52 +0900

    생활코딩

---
+++ nadocoding/python_ruby/Number/4. 변수.py
@@ -0,0 +1,21 @@
+### 변수의 기본문법
+x=10
+y=5
+print(x+y)
+
+title = "python & ruby"
+print("Title is" + title) 
+
+## 문자역에서의 변수의 사용
+# 변수를 왜 사용하는가?
+
+name = "해리포터"
+print("안녕하세요."+name+"님")
+print(name+"님을 위한 강의를 준비했어요")
+print(name+"참석 부탁드립니다.")
+
+### 수 계산에서 변수의 사용
+donation = 200
+student = 10
+sponsor = 20
+print((donation*student)/sponsor)
