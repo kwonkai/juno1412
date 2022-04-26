1350329d2fe692b9f0e30afc66c17c9ad306b0cc
kwon <kwonkai383838@gmail.com>
2022-04-26 18:14:18 +0900

    주석

---
+++ nadocoding/python_ruby/Number/9.주석.py
@@ -0,0 +1,26 @@
+### 주석
+
+# 주석에 준하는 효과를 받을 수 있다.
+''' 
+주석 예제
+작성자
+작성일
+'''
+
+# str = ''' ''' 은 문자열로 표시할 수 있다.
+str = '''
+문자열 예제
+easy
+2022
+'''
+
+# 로그인 어플리케이션 입력 기능 추가
+in_str = input("아이디를 입력해주세요. \n")
+real_egoing=11
+# real_k8805 = "kc"
+if real_egoing == input:
+    print("Hello, egoing")
+# elif real_k8805 == input:
+    # print("Hello_k8805")
+else:
+    print("who are you!")
