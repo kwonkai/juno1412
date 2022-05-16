890af4b4d44d6a422699920ea11f1b0230524666
kwonjh1412 <kwonkai383838@gmail.com>
2022-05-16 23:39:52 +0900

    modules

--- nadocoding/python_ruby/Number/modules/auth.py
+++ nadocoding/python_ruby/Number/modules/auth.py
@@ -1,8 +1,27 @@
 ### modules - 로그인 애플리케이션 적용
-def login(_id):
+def login(_id, _pwd):
     members = ['kim', 'gong', 'lee']
+    passwords = ['1111','2222','3333']
     for member in members:
         if member == _id:
-            return True
+            if _pwd in passwords:       
+                return True
+            else:
+                return False
     return False
 
+### modules - 로그인 애플리케이션 적용
+# def login1(_id, _pwd):
+#     members = ['kim', 'gong', 'lee']
+#     passwords = [1111,2222,3333]
+#     for member in members:
+#         for password in passwords:
+#             if member == _id and password == _pwd:    
+#                 return True
+#     return False
+# def login(_id):
+#     members = ['egoing', 'k8805', 'leezche']
+#     for member in members:
+#         if member == _id:
+#             return True
+#     return False
