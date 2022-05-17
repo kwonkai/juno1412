9203a02b354170fbd5929b7b4959b007aa268483
kwon <kwonkai383838@gmail.com>
2022-05-17 19:39:47 +0900

    linear regresttion

--- DL/모두의 딥러닝/5.linear_regression/5.DL6_linear_regression.py
+++ DL/모두의 딥러닝/5.linear_regression/5.DL6_linear_regression.py
@@ -2,6 +2,7 @@
 # 선형회귀를 이용한 predict
 
 # 라이브러리 설정
+from re import T
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -28,8 +29,6 @@ Y = dataset[:, 13]
 # 훈련셋, 테스트셋 구분
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=seed)
 
-
-
 # 딥러닝 모델 구축
 model = Sequential()
 model.add(Dense(30, input_dim=13, activation='relu'))
@@ -43,10 +42,21 @@ model.fit(x_train, y_train, epochs=200, batch_size = 10)
 # 결론 도출
 # 실제값 VS 예측값
 Y_prediction = model.predict(x_test).flatten()
+Y_prediction = sorted(Y_prediction, reverse=True)
+y_test = sorted(y_test, reverse=True)
 for i in range(10):
     label = y_test[i]
     prediction = Y_prediction[i]
     print("실제가격: {:.3f}, 예상가격: {:.3f}".format(label, prediction))
+
+import matplotlib.pyplot as plt
+plt.scatter(y_test,Y_prediction) # y_test 실제값
+plt.xlabel("true house price")
+plt.ylabel("predict price")
+plt.show()
+
+
+
 
 
 # 선형회귀 모델
@@ -54,11 +64,14 @@ from sklearn.linear_model import LinearRegression
 
 lr = LinearRegression()
 lr.fit(x_train, y_train)
+Y = sorted(Y, reverse=True)
 
 y_predict = lr.predict(x_test)
-y_predict[:10]
+y_predict = sorted(y_predict, reverse=True)
+y_predict
 
 import matplotlib.pyplot as plt
+
 
 for i in range (10):
     real = Y[i]
