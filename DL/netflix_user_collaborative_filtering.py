b3c5de3ac1254c20081f70b299fdbb437f7fcb5a
kwonkai <89757727+kwonkai@users.noreply.github.com>
2022-06-24 17:08:42 +0900

    userbased_collaborative filtering

--- DL/netflix_user_collaborative_filtering.py
+++ DL/netflix_user_collaborative_filtering.py
@@ -8,16 +8,18 @@ import math
 import re
 
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 
 from scipy.sparse import csr_matrix
 from surprise import Reader, Dataset, SVD
 from surprise.model_selection import cross_validate # cross_validate = evaluate -> surprise 버전 변경으로 변경됨
-sns.set_style("ticks")
+#sns.set_style("ticks")
+
+from sklearn.metrics.pairwise import cosine_similarity
 
 # 데이터셋 가져오기
 
-user_data1 = pd.read_csv('./drive/MyDrive/netflix_recommendation/combined_data_1.txt', header = None, names=['user_id', 'Rating'], usecols = [0,1])
+user_data1 = pd.read_csv('/home/juno/workspace/user_collaborative_filtering/data_files/combined_data_1.txt', header = None, names=['user_id', 'Rating'], usecols = [0,1])
 
 user_data1['Rating'] = user_data1['Rating'].astype(float)
 
@@ -52,7 +54,7 @@ for i in range(1,6):
 
 # 데이터 전처리
 
-data_nan = pd.DataFrame(pd.isnull(user_data1.Rating))
+data_nan = pd.DataFrame(pd.isnull(user_data1.Rating)) # 
 data_nan = data_nan[data_nan['Rating'] == True]
 data_nan = data_nan.reset_index()
 print(data_nan)
@@ -60,28 +62,38 @@ print(data_nan)
 movie_np = []
 movie_id = 1
 
+# data_nan check
+a = data_nan['index'][1:]
+b = data_nan['index'][:-1] # 마지막 값 빼고 들고옴 4499 -> 추가 보충 필요
+len(user_data1) - data_nan.iloc[-1, 0] - 1
+
 for i,j in zip(data_nan['index'][1:], data_nan['index'][:-1]):
     temp = np.full((1,i-j-1), movie_id)
     movie_np = np.append(movie_np, temp)
     movie_id += 1
 
-
-record = np.full((1,len(user_data1) - data_nan.iloc[-1, 0] - 1), movie_id)
-movie_np = np.append(movie_np, record)
+# user_data1 index 0~4498 # movie_np index 1~4498 -> 4499 index 추가 필요
+record = np.full((1,len(user_data1) - data_nan.iloc[-1, 0] - 1), movie_id) # 428 # 4499번째 개수 맞음을 확인함
+print(record)
+movie_np = np.append(movie_np, record) # 
+print(movie_np)
 
 print('Movie numpy: {}'.format(movie_np))
 print('Length: {}'.format(len(movie_np)))
 
 # remove those Movie ID rows
+# 결측치가 있는 row 제거
 df = user_data1[pd.notnull(user_data1['Rating'])]
 
+# 컬럼 = movie_id 추가
 df['movie_id'] = movie_np.astype(int)
 df['user_id'] = df['user_id'].astype(int)
 print('-Dataset examples-')
 print(df.iloc[::5000000, :])
 
-## 데이터 유효값 전처리
 
+
+## 데이터 유효값 전처리
 # 1) 리뷰가 너무 적은 영화 제거
 # 2) 리뷰를 너무 적게 제공하는 고객 제거
 
@@ -89,58 +101,64 @@ number = ['count', 'mean']
 
 
 # movie/user 최소 유효값 구하기
+# movie_summary = index.movie_id, columns = count, mean
 movie_summary = df.groupby('movie_id')['Rating'].agg(number)
 movie_summary.index = movie_summary.index.map(int)
 
 
-movie_mark = round(movie_summary['count'].quantile(0.7),0) # quantile 분위값 구하기
+# 분위값 구한 뒤 반올림하기
+movie_mark = round(movie_summary['count'].quantile(0.8),0) 
 remove_movie_list = movie_summary[movie_summary['count'] < movie_mark].index
-
+len(remove_movie_list)
 print('Movies minimum times of review: {}'.format(movie_mark))
 
 
 # user 최소 유효값 구하기
 user_summary = df.groupby('user_id')['Rating'].agg(number)
 user_summary.index = user_summary.index.map(int)
+  
 
-
-user_mark = round(user_summary['count'].quantile(0.7),0) # quantile 분위값 구하기
+user_mark = round(user_summary['count'].quantile(0.8),0) # quantile 분위값 구하기
 remove_user_list = user_summary[user_summary['count'] < user_mark].index
-
+len(remove_user_list)
 print('Users minimum times of review: {}'.format(user_mark))
 
 
 # 데이터 소거
-print('Original Shape: {}'.format(df.shape))
+# remove user/movie list만 가져오기
+# 유효 기준치를 통과한 데이터
 df = df[~df['movie_id'].isin(remove_movie_list)]
 df = df[~df['user_id'].isin(remove_user_list)]
-print('After Trim Shape: {}'.format(df.shape))
-
 print('-Data Examples-')
 print(df.iloc[::5000000, :])
 
 ## 데이터 셋 피벗테이블 정리
-
 df_pivot = pd.pivot_table(df,values = 'Rating', index = 'user_id', columns = 'movie_id')
 print(df_pivot.shape)
 
 ## 데이터 mapping
-movie_title = pd.read_csv('/content/drive/MyDrive/netflix_recommendation/movie_titles.csv', encoding = 'ISO-8859-1', header = None, names = ['movie_id', 'year', 'name'])
+movie_title = pd.read_csv('/home/juno/workspace/user_collaborative_filtering/data_files/movie_titles.csv', encoding = 'ISO-8859-1', header = None, names = ['movie_id', 'year', 'name'])
 
 movie_title.set_index('movie_id', inplace = True)
 print(movie_title.head(10))
 
-reader = Reader()
-
+# index (0, 1, 2) # rating_scale =(1, 5)
+# Reader() # user ; item ; rating ; [timestamp]
+reader = Reader() 
+reader = Reader(rating_scale=(1,5))  # parameter error
 # 10만개 데이터만 가져오기 -> 속도
+# reader 기준으로 데이터를 파싱
+# 학습용 데이터
 m_data = Dataset.load_from_df(df[['user_id', 'movie_id', 'Rating']][:100000], reader)
 
-# cross_val_score()
-
+#  svd() + crossvalidate(algo, m_data, loss, fold = 5
 algo = SVD()
-cross_validate(algo, m_data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+cross_validate(algo, m_data, measures=['RMSE', 'MAE'], cv=5, verbose=True) # ??? cross_validation why? # just loss??
 
-df_svd = df[(df['movie_id'] == 4496) & (df['Rating'] == 5)]
+
+# just check
+# 
+df_svd = df[(df['user_id'] == 4499) & (df['Rating'] == 5)]
 df_svd = df_svd.set_index('movie_id')
 df_svd = df_svd.join(movie_title)['name']
 print(df)
@@ -148,38 +166,77 @@ print(movie_title)
 print(df_svd)
 # print(df_svd)
 
+# user_svd = movie_title raw data -> 유효값 필터링
 user_svd = movie_title.copy()
 user_svd = user_svd.reset_index()
 user_svd = user_svd[~user_svd['movie_id'].isin(remove_movie_list)]
 
-#
+# 코드분석 필요
 user_data = Dataset.load_from_df(df[['user_id', 'movie_id', 'Rating']], reader)
+#전체데이터 학습이용
 trainset = user_data.build_full_trainset()
 algo.fit(trainset)
 
 print(user_svd)
 
-algo.predict(1, 2314, 2)
+algo.predict(1, 2314, 2) # predict(user_id, movie_id, real_rating, predict_rating)
 
-user_svd['Estimate_Score'] = user_svd['movie_id'].apply(lambda x: algo.predict(785314, x).est)
+## surprise 라이브러리에서
+# predict = uid, iid, r_id(실제 평가), est(예측 평가)
+user_svd['Prediction'] = user_svd['movie_id'].apply(lambda x: algo.predict(785314, x).est)
 
 user_svd = user_svd.drop('movie_id', axis = 1)
 
-user_svd = user_svd.sort_values('Estimate_Score', ascending=False)
+user_svd = user_svd.sort_values('Prediction', ascending=False)
 print(user_svd.head(10))
 
 # 예측 점수 추천시스템
 # 분석 # ??
 
-def recommend(title, min_count):
+## 데이터 mapping
+# movie_titles.csv 가져오기
+movie_title = pd.read_csv('/home/juno/workspace/user_collaborative_filtering/data_files/movie_titles.csv', encoding = 'ISO-8859-1', header = None, names = ['movie_id', 'year', 'name'])
+movie_title.set_index('movie_id', inplace = True)
+
+def recommend(title, min_count): # movie_title = title, min_count 
     print("For movie ({})".format(movie_title))
     print("- Top 10 movies recommended based on Pearsons'R correlation - ")
-    i = int(movie_title.index[movie_title['name'] == movie_title][0])
-    target = df_pivot[i]
-    similar_to_target = df_pivot.corrwith(target)
+    i = int(movie_title.index[movie_title['name'] == title][0]) 
+    target = df_pivot[i] # pivot_table target
+    similar_to_target = df_pivot.corrwith(target) # pearson R similarity = target_movie / every movie # corrwith()모든 변수간 상관계수
     corr_target = pd.DataFrame(similar_to_target, columns = ['PearsonR'])
-    corr_target.dropna(inplace = True)
+    corr_target.dropna(inplace = True) # 결측치 제거
     corr_target = corr_target.sort_values('PearsonR', ascending = False)
     corr_target.index = corr_target.index.map(int)
-    corr_target = corr_target.join(movie_title).join(movie_summary)[['PearsonR', 'Name', 'count', 'mean']]
+    corr_target = corr_target.join(movie_title).join(movie_summary)[['PearsonR', 'name', 'count', 'mean']]
     print(corr_target[corr_target['count']>min_count][:10].to_string(index=False))
+
+recommend("Batman Begins", 0)
+
+recommend("Something's Gotta Give", 0)
+
+recommend("X2: X-Men United", 0)
+
+
+def recommend(title, min_count): # movie_title = title, min_count 
+    print("For movie ({})".format(movie_title))
+    print("- Top 10 movies recommended based on Pearsons'R correlation - ")
+    i = int(movie_title.index[movie_title['name'] == title][0]) 
+    target = df_pivot[i] # pivot_table target
+    other_user = df_pivot[df_pivot.index != target]
+    # other_user = df_pivot[~df_pivot.isin(target)]/
+    sim_co = cosine_similarity(target, other_user)
+    corr_target = pd.DataFrame(sim_co, columns = ['cosine_similarity'])
+    corr_target.dropna(inplace = True) # 결측치 제거
+    corr_target = corr_target.sort_values('cosine_similarity', ascending = False)
+    corr_target.index = corr_target.index.map(int)
+    corr_target = corr_target.join(movie_title).join(movie_summary)[['PearsonR', 'name', 'count', 'mean']]
+    print(corr_target[corr_target['count']>min_count][:10].to_string(index=False))
+
+
+
+recommend("Batman Begins", 0)
+
+recommend("Something's Gotta Give", 0)
+
+recommend("X2: X-Men United", 0)
--- DL/sim_person_&_recommend_anime.py
+++ DL/sim_person_&_recommend_anime.py
@@ -1,5 +1,5 @@
 # Commented out IPython magic to ensure Python compatibility.
-# 라이브러리 설정
+# 1. 라이브러리 설정
 import random
 import pandas as pd
 import statistics
@@ -9,86 +9,81 @@ import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
 import operator
 
-# 데이터 불러오기
+# 2. 데이터 불러오기 & 확인 & 시각화
 
-anime = pd.read_csv('/content/drive/MyDrive/anime/anime.csv')
-rating = pd.read_csv('/content/drive/MyDrive/anime/rating.csv')
+anime = pd.read_csv('/home/juno/workspace/user_collaborative_filtering/data_files/anime/anime.csv')
+rating = pd.read_csv('/home/juno/workspace/user_collaborative_filtering/data_files/anime/rating.csv')
 
-rating.shape
 
-anime.shape
 
-# 데이터 확인
-
-rating.head(10)
-
-anime.head(10)
-
-# number of ratings
+# 데이터 숫자/고유값
 len(rating)
-
 # number of users
 len(rating['user_id'].unique())
-
 # number of unique animes (in anime list, not ratings)
 len(anime['anime_id'].unique())
 
-# 콘텐츠를 평가한 유저 숫자
 
+# 데이터 시각화
+# 콘텐츠를 평가한 유저 숫자
 rating_per_user = rating.groupby('user_id')['rating'].count()
-statistics.mean(rating_per_user.tolist())
+statistics.mean(rating_per_user.tolist()) # 1인당 106.28765558049378
 
 # 유저별 평점 개수의 분포
 rating_per_user.hist(bins=20, range=(0,500))
 
 # 콘텐츠별 평가된 평점 등급
 ratings_per_anime = rating.groupby('anime_id')['rating'].count()
-statistics.mean(ratings_per_anime.tolist())
+statistics.mean(ratings_per_anime.tolist()) # 1콘텐츠당 697.3번 리뷰
 
 # 콘텐츠별 평가된 빈도
 ratings_per_anime.hist(bins=30, range=(0,5000))
 
+
+
+
+# 3. 데이터 전처리
+# rating 평점 '-1' -> '5'으로 변경 = normal
+# -1 : 보았으나 평가하지 않은 데이터
+rating = rating.replace({'rating':-1},5)
+
+
 # rating(평가된 콘텐츠 전처리)
-# 너무 적게 평가된 콘텐츠 제거
-# counts of ratings per anime as a df
+# 너무 적게 평가된 콘텐츠 제거(50개 이하 평가 데이터 제거)
 ratings_per_anime_df = pd.DataFrame(ratings_per_anime)
 
-# 평점 개수가 100개 이하 콘텐츠 제거
-filtered_ratings_per_anime_df = ratings_per_anime_df[ratings_per_anime_df.rating >= 500]
-print(filtered_ratings_per_anime_df)
-
-# df -> list
+filtered_ratings_per_anime_df = ratings_per_anime_df[ratings_per_anime_df.rating >= 50]
 popular_anime = filtered_ratings_per_anime_df.index.tolist()
+len(filtered_ratings_per_anime_df)
 
 # anime(콘텐츠를 평가한 유저 데이터 처리)
-# 너무 적게 평가한 유저 제거
-# counts ratings per user as a df
+# 너무 적게 평가한 유저 제거 -> 30개 이하 평가한 유저 제거
 rating_per_user_df = pd.DataFrame(rating_per_user)
-# remove if < 500
-filtered_rating_per_user_df = rating_per_user_df[rating_per_user_df.rating >= 200]
-print(filtered_rating_per_user_df)
-# build a list of user_ids to keep
-prolific_users = filtered_rating_per_user_df.index.tolist()
+filtered_rating_per_user_df = rating_per_user_df[rating_per_user_df.rating >= 30]
+rating_users = filtered_rating_per_user_df.index.tolist()
+len(filtered_rating_per_user_df)
 
 filtered_rating = rating[rating.anime_id.isin(popular_anime)]
-filtered_rating = rating[rating.user_id.isin(prolific_users)]
+filtered_rating = rating[rating.user_id.isin(rating_users)]
 len(filtered_rating)
 
-# 유저 리스트 만들기
+# 유저 리스트 만들기 # 랜덤 유저 뽑아내는 용도
 user_id_list = list(set(filtered_rating['user_id']))
-len(user_id_list)
 
-# 피벗테이블 만들기
+
+
+
+# 4. 피벗테이블 만들기
 rating_matrix = filtered_rating.pivot_table(index='user_id', columns='anime_id', values='rating')
 
 # 결측치 제거
 rating_matrix = rating_matrix.fillna(0)
-rating_matrix.head(15)
 
-# 비슷한 유저 찾기
-# 코드 분석
+
+
+# 5. 비슷한 유저 찾기
 def similar_users(user_id, matrix, k=5):
-    # 현재 유저에 대한 데이터프레임 만들기
+    # 현재 유저에 대한 정보찾기
     user = matrix[matrix.index == user_id]
     
     # matrix index 값이 user_id와 다른가?
@@ -114,56 +109,106 @@ def similar_users(user_id, matrix, k=5):
     
     return users
 
+
 # 유저정보 list 뽑기
-# 코드 제작 not 변수
-random_user_id = random.choice(user_id_list)
-
-# 콜  비슷한 유저
-similar_user_indices = similar_users(random_user_id, rating_matrix)
-print(similar_user_indices)
+# # 코드 제작 not 변수 -> input or user searching 함수 제작
+# random_user_id = random.choice(user_id_list)
 
 
-# 콘텐츠 추천
+# 특정 유저에게 콘텐츠 추천
+recommend_user = 315
+
+# # call similarity user
+# similar_user_indices = similar_users(random_user_id, rating_matrix)
+
+# call similarity user
+similar_user_indices = similar_users(recommend_user, rating_matrix)
+
+
+
+# 7. 콘텐츠 추천
 # 코드 분석
 def recommend_item(user_index, similar_user_indices, matrix, items=10):
-    
-    # load vectors for similar users
+    # 유저와 비슷한 유저 가져오기
     similar_users = matrix[matrix.index.isin(similar_user_indices)]
-    # calc avg ratings across the 3 similar users
+    # 비슷한 유저 평균 계산 # row 계산
     similar_users = similar_users.mean(axis=0)
-    # convert to dataframe so its easy to sort and filter
+    # dataframe 변환 : 정렬/필터링 용이
     similar_users_df = pd.DataFrame(similar_users, columns=['mean'])
-    
-    
-    # load vector for the current user
+
+
+    # 현재 사용자의 벡터 가져오기 : matrix = rating_matrix(pivot table)
     user_df = matrix[matrix.index == user_index]
-    # transpose it so its easier to filter
+
+    # 현재 사용자의 평가 데이터 정렬
     user_df_transposed = user_df.transpose()
-    # rename the column as 'rating'
+
+    # 컬럼명 변경 user_id : 48432 -> rating
     user_df_transposed.columns = ['rating']
-    # remove any rows without a 0 value. Anime not watched yet
+
+    # 미시청 콘텐츠는 rating 0로 바꾸어 준다. remove any rows without a 0 value. Anime not watched yet
     user_df_transposed = user_df_transposed[user_df_transposed['rating']==0]
-    # generate a list of animes the user has not seen
+
+    # 미시청 콘텐츠 목록리스트 만들기
     animes_unseen = user_df_transposed.index.tolist()
-    
-    # filter avg ratings of similar users for only anime the current user has not seen
+
+    # 안본 콘텐츠 필터링
     similar_users_df_filtered = similar_users_df[similar_users_df.index.isin(animes_unseen)]
-    # order the dataframe
-    similar_users_df_ordered = similar_users_df.sort_values(by=['mean'], ascending=False)
-    # grab the top n anime   
+
+    # 평균값을 내림차순 정렬
+    # sort_values(by=[정렬 기준 축])
+    # 'mean' = similar_user의 columns의 이름
+    similar_users_df_ordered = similar_users_df_filtered.sort_values(by=['mean'], ascending=False)
+
+    # 상위 10개 값 가져오기
+    # items = 10
     top_n_anime = similar_users_df_ordered.head(items)
     top_n_anime_indices = top_n_anime.index.tolist()
-    # lookup these anime in the other dataframe to find names
+
+    # anime dataframe에서 top10값 찾기
     anime_information = anime[anime['anime_id'].isin(top_n_anime_indices)]
+    anime_information
     
     return anime_information #items
 
+
+
 # 유저정보 list 뽑기
 # 유저정보 뽑기
-random_user_id = random.choice(user_id_list)
-print(random_user_id)
-# recommend_user = ""
+# random_user_id = random.choice(user_id_list)
+# print(random_user_id)
+
+# 추천 콘텐츠 뽑아내기 # 랜덤
+# recommend_content = recommend_item(random_user_id, similar_user_indices, rating_matrix)
 
 
-# 추천 아이템 뽑아내기
-recommend_item(random_user_id, similar_user_indices, rating_matrix)
+
+# 특정 유저에게 지정
+# recommend_user = 191
+
+# 추천 콘텐츠 뽑아내기 #특정 유저
+recommend_content = recommend_item(recommend_user, similar_user_indices, rating_matrix)
+
+print("-- 콘텐츠 추천 TOP 10 --")
+
+# 모든 추천
+print(recommend_content)
+print("===================================")
+
+# movie_id만 뽑기
+print("-- ID --")
+print(recommend_content['anime_id'])
+print("===================================")
+
+# name만 뽑기
+print("-- 제목 --")
+print(recommend_content['name'])
+print("===================================")
+
+# rating만 뽑기
+print("-- 평점 --")
+print(recommend_content['name'])
+print("===================================")
+# 특정유저에게 콘텐츠 추천학시
+# recommend_content = recommend_item(recommend_user, similar_user_indices, rating_matrix)
+# print(recommend_content)
