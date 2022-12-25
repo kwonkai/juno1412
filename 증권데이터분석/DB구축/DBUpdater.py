592298ef25aa50efafaff17c3feac983706e27af
kwonjh1412 <kwonkai383838@gmail.com>
2022-12-25 21:53:40 +0900

    증권데이터 api 구축

--- 증권데이터분석/DB구축/DBUpdater.py
+++ 증권데이터분석/DB구축/DBUpdater.py
@@ -1,11 +1,9 @@
-﻿# 라이브러리 설정
-import enum
+﻿import pandas as pd
 from bs4 import BeautifulSoup
-import pymysql
-import pandas as pd
-from datetime import datetime
-
+import pymysql, calendar, time, json
 import requests
+from datetime import datetime
+from threading import Timer
 
 class DBUpdater:
     # 생성자 = MariaDB 연결 및 종목코드 딕셔너리 생성
@@ -97,41 +95,30 @@ class DBUpdater:
     # 네이버금융에서 주식 시세를 읽어서 데이터프레임으로 변환
     def read_naver(self, code, company, pages_to_fetch):
         try:
-            url = f'https://finance.naver.com/item/sise_day.nhn?code={code}'
-            html = requests.get(url, headers={'User-agent': 'Mozila/5.0'})
-            bs = BeautifulSoup(html, "lxml")
-            pgrr = bs.find("td", class_="pgRR")
+            url = f"http://finance.naver.com/item/sise_day.nhn?code={code}"
+            html = BeautifulSoup(requests.get(url,
+                headers={'User-agent': 'Mozilla/5.0'}).text, "lxml")
+            pgrr = html.find("td", class_="pgRR")
             if pgrr is None:
                 return None
-            
             s = str(pgrr.a["href"]).split('=')
-
-            # 1. 네이버 금융 일별 시세의 마지막 페이지 탐색
-            lastpage = s[-1]
+            lastpage = s[-1] 
             df = pd.DataFrame()
-
-            # 2. 설정파일에 설정된 페이지수(pages_to_fetch)와 1의 페이지 수에서 작은 것을 선택
             pages = min(int(lastpage), pages_to_fetch)
-
-            # 3. 일별 시세 페이지를 read_html()로 읽어 데이터 프레임에 추가
-            for page in range(1, pages+1):
-                url = '{}&page={}'.format(url, page)
-                req = requests.get(url, headers={'User-agent': 'Mozila/5.0'})
-                df = df.append(pd.read_html(req.text, header=0)[0])
-                tmnow = datetime.now().strftime('&Y-%m-%d %H %M')
-                print('[{}] {} ({}) : {:04d}/{:04d} pages arae downloading...'.format(tmnow, company, code, page, pages), end="\r")
-                
-                # 4. df dataframe의 columns 명 변경
-                df = df.rename(columns = {'날짜':'date', '종가':'close', '전일비':'diff', '시가':'open', '고가':'high', '저가':'low', '거래량':'volume'})                
-                # 5. 연,월,일 형식의 date 데이터를 연-월-일 형식으로 변경
-                df['date'] = df['date'].replace('.', '-')
-                # 6. 결측치 제거
-                df = df.dropna()
-                # 7. df 데이터프레임 value 값 str -> int로 변경
-                df[['close', 'diff', 'open', 'high', 'low','volume']] = df[['close', 'diff', 'open', 'high', 'low','volume']].astype(int)
-
-                # 8.시간, OHLC, DIFF, 거래량만 가져오기
-                df = df[['open', 'high', 'low', 'close', 'diff', 'volume']]
+            for page in range(1, pages + 1):
+                pg_url = '{}&page={}'.format(url, page)
+                df = df.append(pd.read_html(requests.get(pg_url,
+                    headers={'User-agent': 'Mozilla/5.0'}).text)[0])                                          
+                tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
+                print('[{}] {} ({}) : {:04d}/{:04d} pages are downloading...'.
+                    format(tmnow, company, code, page, pages), end="\r")
+            df = df.rename(columns={'날짜':'date','종가':'close','전일비':'diff'
+                ,'시가':'open','고가':'high','저가':'low','거래량':'volume'})
+            df['date'] = df['date'].replace('.', '-')
+            df = df.dropna()
+            df[['close', 'diff', 'open', 'high', 'low', 'volume']] = df[['close',
+                'diff', 'open', 'high', 'low', 'volume']].astype(int)
+            df = df[['date', 'open', 'high', 'low', 'close', 'diff', 'volume']]
         
         except Exception as e:
             print('Exception occured :', str(e))
@@ -146,14 +133,14 @@ class DBUpdater:
 
     # 네이버 금융에서 읽어온 주식 시세를 DB에 REPLACE
     def replace_into_db(self, df, num, code, company):
-        with self.conn.consor() as curs:
+        with self.conn.cursor() as curs:
             # 1. 인수로 넘겨받은 dataframe을 tuple로 순회처리한다.
             for r in df.itertuples():
                 # 2. REPLACE INTO 구문으로 daily_price 테이블 업데이트
                 # 값이 string 이면 '{}', int 이면 {}
-                sql = f"REPLACE INTO daily_price VALUES"\
-                    f"('{code}', '{r.date}', {r.open}, {r.high}, {r.low}, {r.close}" \
-                    f"{r.diff}, {r.volume}"
+                sql = f"REPLACE INTO daily_price VALUES ('{code}', "\
+                    f"'{r.date}', {r.open}, {r.high}, {r.low}, {r.close}, "\
+                    f"{r.diff}, {r.volume})"
                 curs.execute(sql)
             
             # 3. commit() 함수로 maria DB에 반영한다.
@@ -179,7 +166,50 @@ class DBUpdater:
 
     # 실행 즉시 매일 오후 5시에 daily_price 테이블 업데이트
     def execute_daily(self):
+        # 1. update_comp_info() method를 호출 해 상장법인 목록을 DB에 업데이트 한다.
+        self.update_comp_info()
+
+        # 2. DBUpdater.py가 있는 디렉터리에서 config.json 파일을 읽기모드로 열어준다.
+        # 3. 파일이 있다면 page_to_fetch 값을 읽어서 사용한다.
+        try:
+            with open('config.json','r') as in_file:
+                config = json.load(in_file)
+                pages_to_fetch = config['pages_to_fetch']
+        
+        # 4. 파일이 없다면 config.json 파일을 생성해준다. 처음 생성 시 page_to_fetch 100, 이후 1로 설정
+        except FileNotFoundError:
+            with open('config.json', 'w') as out_file:
+                pages_to_fetch = 100
+                config = {'pages_to_fetch': 1}
+                json.dump(config, out_file)
+
+        # 5. pages_to_fetch 값으로 update_daily_price method를 호출한다.
+        self.update_daily_price(pages_to_fetch)
+
+        # 6. 이번달 마지막날(lastday)을 구해 다음날 오후 5시를 계산한다.
+        tmnow = datetime.now()
+        lastday = calendar.monthrange(tmnow.year, tmnow.month)[1]
+        if tmnow.month == 12 and tmnow.day == lastday:
+            tmnext = tmnow.replace(year=tmnow.year+1, month=1, day=1, hour=17, minute=0, second=0)
+        elif tmnow.month == lastday:
+            tmnext = tmnow.replace(month=tmnow.month+1, day=1, hour=17, minute=0, second=0)
+        else:
+            tmnext = tmnow.replace(day = tmnow.day+1, hour=17, minute=0, second=0)
+        
+        tmdiff = tmnext - tmnow
+        secs = tmdiff.seconds
+
+        # 7. 다음날 오후 5시에 excute_daily() method를 실행하는 타이머 객체를 설정한다.
+        t = Timer(secs, self.execute_daily)
+        print("Waiting for next update ({}) ...".format(tmnext.strftime('%Y-%m-%d %H:%M')))
+
+        t.start()
+        
+
+
+
+
 
 if __name__ == '__main__':
     dbu = DBUpdater()
-    dbu.update_comp_info()
+    dbu.execute_daily()
